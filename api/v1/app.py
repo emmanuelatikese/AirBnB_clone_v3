e30bfd73fe542667983c4cd9c4ed8da8e4637fd9
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-02-05 01:42:26 +0000

    adding

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' This is something'''
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,8 +14,12 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def checkError(self):
+def tear_down(self):
     storage.close()
+
+@app.errorhandler(404)
+def checkError404(error):
+	return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
     app.run(host=Host, port=int(Port), threaded=True)
