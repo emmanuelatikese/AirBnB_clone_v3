a883a47da6b3fe9925cde0324f304e640c9af91a
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-02-05 03:46:15 +0000

    adding

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,37 +8,55 @@ from models.base_model import BaseModel, Base
 from models import storage
 from flask import request, jsonify, abort
 
-@app_views.route('/states', methods=['GET', 'POST'])
-@app_views.route('/states/<state_id>', methods['GET', 'DELETE'])
+
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
 def base_id(state_id=None):
-	get_all = storage.all(State)
-	if request.method == "GET":
-		if state_id:
-			try:
-				k = states+'.'+ escape(state_id)
-				return jsonify(get_all[k].to_dict())
-			except KeyError:
-				abort(404)
-		else:
-			l = [ v.to_dict for k, v in get_all.items() ]
-			return jsonify(l)
-	if request.method == "DELETE":
-		if state_id:
-                        try:
-                                k = states+'.'+ escape(state_id)
-                                storage.delete(get_all[k].to_dict)
-				storage.save()
-				return jsonify({}), 200
-                        except KeyError:
-                                abort(404)
-	if request.method == "POST":
-		if request.is_json:
-			json_get = request.get_json()
-		else:
-			abort(400, "Not a JSON")
-		if json_get['name']:
-			new = storage.new(State(**json_get))
-			storage.save()
-			return jsonify(new.to_dict()), 201
-		else:
-			abort(400, 'Missing name') 
+    get_all = storage.all(State)
+    if request.method == "GET":
+        if state_id:
+            try:
+                k = 'State'+'.' + state_id
+                return jsonify(get_all[k].to_dict())
+            except KeyError:
+                abort(404)
+        else:
+            l = [v.to_dict() for k, v in get_all.items()]
+            return jsonify(l)
+    if request.method == "DELETE":
+        if state_id:
+            try:
+                k = 'State'+'.' + escape(state_id)
+                storage.delete(get_all[k])
+                storage.save()
+                return jsonify({}), 200
+            except KeyError:
+                abort(404)
+    if request.method == "POST":
+        if request.is_json:
+            json_get = request.get_json()
+        else:
+            abort(400, "Not a JSON")
+        if json_get['name']:
+            new_inst = State(**json_get)
+            storage.new(new_inst)
+            storage.save()
+            return jsonify(new_inst.to_dict()), 201
+        else:
+            abort(400, 'Missing name')
+
+    if request.method == "PUT":
+        try:
+            k = 'State.' + state_id
+            new = get_all[k]
+            if request.is_json:
+                json_get = request.get_json()
+            else:
+                abort(400, "Not a JSON")
+            for k, v in json_get.items():
+                if k != 'id' and k != 'created_at' and k != 'updated_at':
+                    setattr(State, k, v)
+            storage.save()
+            return jsonify(new.to_dict()), 200
+        except KeyError:
+            abort(404)
